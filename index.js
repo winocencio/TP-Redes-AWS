602426041650fde43d9b358a8e25c03e83a83339
Willian Inocencio <willian.i.alves@hotmail.com>
2019-11-12 20:52:47 -0300

    mudanca de porta

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('<img src="https://i1.rgstatic.net/ii/profile.image/277080219635712-1443072480340_Q512/Claudio_Nunes3.jpg" > </img> <h1>Hello Claudion!</h1>');
 });
 
-app.listen(3000, function () {
-    console.log('Conectado na porta 3000!');
+app.listen(80, function () {
+    console.log('Conectado na porta 80!');
   });
